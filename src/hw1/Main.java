3fc7acaf7920c4930aac16982fc10e3ee19a01aa
Nikita Kurpatow <kurpatowph@yandex.ru>
2022-09-23 15:11:57 +0300

    add task number 3

--- src/hw1/Main.java
+++ src/hw1/Main.java
@@ -2,11 +2,16 @@ package hw1;
 
 public class Main {
     public static void main(String[] args) {
-        //Задание№1
-        int a = 5, b = 10, c = 5;
-        System.out.println(a * b * c);
-        //Задание№2
-        int d = 9;
-        System.out.println(d * d * d);
+        //Задание №1
+        int length = 5, width = 10, height = 5;
+        System.out.println("Площадь равна " + length * width * height);
+        //Задание №2
+        int number = 9;
+        System.out.println("Куб числа " + number + " равен " + number * number * number);
+        //Задание №3
+        double time = 7;
+        double distance = 600;
+        double mc = (distance / time) / 3.6;
+        System.out.println("Итого метров секунду " + mc);
     }
 }
