c01d8f53316e4ad3af72eab200877b946dd6cf46
Nikita Kurpatow <kurpatowph@yandex.ru>
2022-09-23 14:52:12 +0300

    add task number 2

--- src/hw1/Main.java
+++ src/hw1/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         //Задание№1
         int a = 5, b = 10, c = 5;
-        System.out.println(a*b*c);
+        System.out.println(a * b * c);
         //Задание№2
+        int d = 9;
+        System.out.println(d * d * d);
     }
 }
