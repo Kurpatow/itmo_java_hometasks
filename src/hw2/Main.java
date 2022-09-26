0fa666bf05d43784d569c1ab6b4e86ef76d17ac8
Nikita Kurpatow <kurpatowph@yandex.ru>
2022-09-26 16:30:07 +0300

    add first task

---
+++ src/hw2/Main.java
@@ -0,0 +1,46 @@
+package hw2;
+
+public class Main {
+    public static void main(String[] args) {
+        //Задача №1
+        int numberOfMonth = 1;
+        switch (numberOfMonth){
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 11:
+                System.out.println("Осень");
+                break;
+            case 10:
+                System.out.println("Осень");
+                break;
+            case 9:
+                System.out.println("Осень");
+                break;
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 7:
+                System.out.println("Лето");
+                break;
+            case 6:
+                System.out.println("Лето");
+                break;
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 4:
+                System.out.println("Весна");
+                break;
+            case 3:
+                System.out.println("Весна");
+                break;
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 1:
+                System.out.println("Зима");
+                break;
+        }
+    }
+}
