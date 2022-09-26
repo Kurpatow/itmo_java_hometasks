f2fd95c87419909c543d05709424c5a97120d4e3
Nikita Kurpatow <kurpatowph@yandex.ru>
2022-09-26 16:39:01 +0300

    add first task

--- src/hw2/Main.java
+++ src/hw2/Main.java
@@ -3,44 +3,21 @@ package hw2;
 public class Main {
     public static void main(String[] args) {
         //Задача №1
-        int numberOfMonth = 1;
-        switch (numberOfMonth){
-            case 12:
-                System.out.println("Зима");
-                break;
-            case 11:
-                System.out.println("Осень");
-                break;
-            case 10:
-                System.out.println("Осень");
-                break;
-            case 9:
-                System.out.println("Осень");
-                break;
-            case 8:
-                System.out.println("Лето");
-                break;
-            case 7:
-                System.out.println("Лето");
-                break;
-            case 6:
-                System.out.println("Лето");
-                break;
-            case 5:
-                System.out.println("Весна");
-                break;
-            case 4:
-                System.out.println("Весна");
-                break;
-            case 3:
-                System.out.println("Весна");
-                break;
-            case 2:
-                System.out.println("Зима");
-                break;
-            case 1:
-                System.out.println("Зима");
-                break;
+        int month = 13;
+
+        if (month == 12 || month == 1 || month == 2) {
+            System.out.println("Зима");
+        } else if (month == 3 || month == 4 || month == 5) {
+            System.out.println("Весна");
+        } else if (month == 6 || month == 7 || month == 8) {
+            System.out.println("Лето");
+        } else if (month == 9 || month == 10 || month == 11) {
+            System.out.println("Осень");
+        } else {
+            System.out.println("Несуществующее время года");
         }
+
+        //Задание №2
+
     }
 }
