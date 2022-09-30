7f00e1aa285650596c97c965501dbda997e2962d
Nikita Kurpatow <kurpatowph@yandex.ru>
2022-09-30 21:56:43 +0300

    add hw4

---
+++ src/hw4/Main.java
@@ -0,0 +1,29 @@
+package hw4;
+
+public class Main {
+    public static void main(String[] args) {
+        /* Разобрать следующие методы строк:
+
+        replaceAll / replaceFirst
+
+        contains / startsWith / endsWith
+
+        trim / strip / stripIndent / stripLeading / stripTrailing
+
+        compareTo / compareToIgnoreCase
+
+        isBlank / isEmpty
+
+        repeat
+
+        toLowerCase / toUpperCase
+
+        charAt
+
+        String cat = " Кот ";
+        cat = cat.trim();  // "Кот"
+
+        boolean res = cat.startsWith("Ко"); // true
+         */
+    }
+}
