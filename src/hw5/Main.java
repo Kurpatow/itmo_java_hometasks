db7e50f6bff3bc41895c4ca598fb1fca87579443
Nikita Kurpatow <kurpatowph@yandex.ru>
2022-10-01 12:37:28 +0300

    add first task

--- src/hw5/Main.java
+++ src/hw5/Main.java
@@ -2,7 +2,7 @@ package hw5;
 
 public class Main {
     public static void main(String[] args) {
-       //Задание №1
+       //Задание №2
         int[] num = new int[10];
         for (int i = 0, b = 2; i < num.length; b = b + 2, i++){
             num[i] = b;
@@ -10,5 +10,7 @@ public class Main {
         for (int i = num.length - 1; i >= 0; i--){
             System.out.println(num[i]);
         }
+        //Задание №1
+
     }
 }
