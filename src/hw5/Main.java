e1e13f01a0bd2e373d0a13dd9133ceb2a180351d
Nikita Kurpatow <kurpatowph@yandex.ru>
2022-10-01 14:11:15 +0300

    add second task

--- src/hw5/Main.java
+++ src/hw5/Main.java
@@ -2,15 +2,41 @@ package hw5;
 
 public class Main {
     public static void main(String[] args) {
-       //Задание №2
-        int[] num = new int[10];
-        for (int i = 0, b = 2; i < num.length; b = b + 2, i++){
-            num[i] = b;
-        }
-        for (int i = num.length - 1; i >= 0; i--){
-            System.out.println(num[i]);
-        }
         //Задание №1
+        int[] nam = new int[4];
+        for (int i = 0; i < nam.length; i++) {
+            nam[i] = (int) (Math.random() * 9);
+            System.out.println(nam[i]);
+        }
+        int max = nam[0];
+        int min = nam[0];
+        int sArf = 0;
+        int i;
+        for (i = 0; i < nam.length; i++) {
+            if (max < nam[i]) {
+                max = nam[i];
+            }
+            if (min > nam[i]){
+                min = nam[i];
+            }
+        }
+        for (int b : nam){
+            sArf += b;
+        }
+        System.out.println("Максимальное число " + max);
+        System.out.println("Минимальное число " + min);
+        System.out.println("Среднее арефмитическое " + sArf / nam.length);
 
+        //Задание №2
+        int[] num = new int[10];
+        for (int j = 0, b = 2; j < num.length; b += 2, j++) {
+            num[j] = b;
+        }
+        for (int j = num.length - 1; j >= 0; j--) {
+            System.out.println(num[j]);
+        }
+        //Задание №3
+        //Задание №4
+        //Задание №5
     }
 }
