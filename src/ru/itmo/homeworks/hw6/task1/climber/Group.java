12114b11c3d3ce0f057f07fcaba8ff3c352f1d68
Nikita Kurpatow <kurpatowph@yandex.ru>
2022-10-05 17:31:04 +0300

    add task 1 hw6

---
+++ src/ru/itmo/homeworks/hw6/task1/climber/Group.java
@@ -0,0 +1,37 @@
+package ru.itmo.homeworks.hw6.task1.climber;
+
+public class Group {
+    private Mountain mountain;
+    private Alpinist[] alpinists;
+    private boolean isOpen = true;
+    private int maxAlpinists = 3;
+    private int numberOfAlpinists = 0;
+
+    public Group(Mountain mountain) {
+        this.mountain = mountain;
+        alpinists = new Alpinist[maxAlpinists];
+    }
+    public Mountain getMountain() {
+        return mountain;
+    }
+    public Alpinist[] getAlpinists() {
+        return alpinists;
+    }
+    public boolean isOpen() {
+        if(isOpen)
+            System.out.println("Набор в группу открыт \n");
+        else
+            System.out.println("Набор в группу закрыт \n");
+        return isOpen;
+    }
+    public void addAplinist(Alpinist alpinist) {
+        if (isOpen) {
+            alpinists[numberOfAlpinists] = alpinist;
+            System.out.println("Альпинист " + alpinist.getName() + " добавлен в группу");
+            numberOfAlpinists++;
+        } else {
+            System.out.println("Альпинист " + alpinist.getName() + " НЕ добавлен в группу!!! \nНабор в группу закрыт \n");
+        }
+        if (numberOfAlpinists == maxAlpinists) isOpen = false;
+    }
+}
