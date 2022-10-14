365ff132b6835be64fa32f78a02e874f8c583bbe
Nikita Kurpatow <kurpatowph@yandex.ru>
2022-10-15 00:25:03 +0300

    add fix

--- src/ru/itmo/homeworks/hw7/Main.java
+++ src/ru/itmo/homeworks/hw7/Main.java
@@ -14,7 +14,7 @@ public class Main {
         School school = new School("Школа 495", 2, 8, 4);
         Teacher teacher1 = new Teacher("Наталья Игоревна", 39, "Алгебра", 90);
         Teacher teacher2 = new Teacher("Ирина Юрьевна", 44, "Геометрия", 88);
-        Teacher teacher3 = new Teacher("Виктор Васильевич", 34, "Физика", 86);
+        Teacher teacher3 = new Teacher("Михаил Васильевич", 34, "Физика", 86);
         Teacher teacher4 = new Teacher("Иван Викторович", 32, "История", 99);
 
         school.addTeacher();
@@ -23,7 +23,7 @@ public class Main {
         school.addTeacher();
 
         for (int i = 0; i < 10; i++) {
-            Student student = new Student(String.format("Ученик %d",i+1), (int) (6 + (Math.random() * 7)),
+            Student student = new Student(String.format("Ученик %d",i + 1), (int) (6 + (Math.random() * 7)),
                     Main.classes[(int) (Math.random() * 2)], (int) (Math.random() * 30));
             school.addStudent();
         }
