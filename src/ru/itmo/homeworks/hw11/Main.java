a23df402fb465453932c1a8b6aa92a951e376997
Nikita Kurpatow <kurpatowph@yandex.ru>
2022-10-15 11:21:20 +0300

    add fix

--- src/ru/itmo/homeworks/hw11/Main.java
+++ src/ru/itmo/homeworks/hw11/Main.java
@@ -4,6 +4,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(SolarSystemPlanets.getSolarSystemPlanets()));
+        System.out.println(Arrays.toString(SolarSystemPlanets.values()));
     }
 }
