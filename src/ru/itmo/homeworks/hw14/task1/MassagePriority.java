f7814a41a805132d745573cfc10270daa706b292
Nikita Kurpatow <kurpatowph@yandex.ru>
2022-10-24 14:45:20 +0300

    add fix

---
+++ src/ru/itmo/homeworks/hw14/task1/MassagePriority.java
@@ -0,0 +1,14 @@
+package ru.itmo.homeworks.hw14.task1;
+
+enum MessagePriority {
+    LOW, MEDIUM, HIGH, URGENT;
+
+    public static MessagePriority getPriority(int ord){
+        for (MessagePriority mp: values()){
+            if (ord == mp.ordinal()) {
+                return mp;
+            }
+        }
+        throw new AssertionError("Wrong ordinal: " + ord);
+    }
+}
