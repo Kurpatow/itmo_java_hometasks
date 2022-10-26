2d959ba90e37cfdabe0901b268a3d1d6535cca8f
Nikita Kurpatow <kurpatowph@yandex.ru>
2022-10-26 16:05:25 +0300

    add fix

--- src/ru/itmo/homeworks/hw14/task1/MessageTask.java
+++ src/ru/itmo/homeworks/hw14/task1/MessageTask.java
@@ -1,15 +1,42 @@
 package ru.itmo.homeworks.hw14.task1;
+import java.util.HashSet;
 import java.util.List;
 
 public class MessageTask {
     public static void countEachPriority(List<Message> messageList) {
         // TODO:  Подсчитать количество сообщений для каждого приоритета
         //  Ответ в консоль
+        int count;
+        System.out.println("Количество сообщений для каждого приоритета");
+        for (MessagePriority md : MessagePriority.values()){
+            count = 0;
+            for (Message message : messageList){
+                if (md.equals(message.getPriority())){
+                    count++;
+                }
+            }
+            System.out.println("Приоритет: " + md + " Колличество сообщений: " + count);
+        }
     }
 
     public static void countEachCode(List<Message> messageList) {
         // TODO: Подсчитать количество сообщений для каждого кода сообщения
         //  Ответ в консоль
+        System.out.println("Количество сообщений для каждого кода сообщения");
+        HashSet<Integer> massageSet = new HashSet<>();
+        int count;
+        for (Message massage : messageList) {
+            massageSet.add(massage.getCode());
+        }
+        for (Integer code : massageSet) {
+            count = 0;
+            for (Message message : messageList) {
+                if (code.equals(message.getCode())){
+                    count ++;
+                }
+            }
+            System.out.println("Код сообщения: " + code + " Колличество сообщений: " + count);
+        }
     }
 
     public static void uniqueMessageCount(List<Message> messageList) {
@@ -38,6 +65,6 @@ public class MessageTask {
 
     public static void main(String[] args) {
         List<Message> messages = Message.generate(34);
-        System.out.println(messages);
+        System.out.println();
     }
 }
