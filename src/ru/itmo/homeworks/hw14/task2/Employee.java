0f6febc96808ed03e38ecc79ea4aea8c05cc0815
Nikita Kurpatow <kurpatowph@yandex.ru>
2022-10-28 16:37:20 +0300

    add fix

--- src/ru/itmo/homeworks/hw14/task2/Employee.java
+++ src/ru/itmo/homeworks/hw14/task2/Employee.java
@@ -10,7 +10,52 @@ public class Employee {
     private int age;
 
     // TODO: добавить конструктор, необходимы геттеры и сеттеры
+    public Employee(String name, String company, int salary, int age) {
+        setName(name);
+        setCompany(company);
+        setSalary(salary);
+        setAge(age);
 
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Сотрудники [ " + "Имя: " + name + ", " +
+                "Компания: " + company + ", " +
+                "Зарплата: " + salary + ", " +
+                "Возраст: " + age + "." + " ]";
+    }
 
     // TODO: дописать реализацию метода для создания списка из объектов класса Employee.
     //  Объекты Employee создавать с рандомными значениями. Возраст от 21 до 60 и не забудьте про зп.
@@ -18,11 +63,22 @@ public class Employee {
     public static List<Employee> employeeGenerator(int num) {
         String[] names = {"Mike", "Tom", "Alex", "John", "Peter", "Jack", "Charlie", "Max", "Jenifer", "Linda", "Elizabeth"}; // массив с именами
         String[] companies = {"Microsoft", "IBM", "Google", "General Electric", "Siemens", "Samsung", "Apple"}; // массив с названиями компаний
+        int minAge = 21;
+        int maxAge = 60;
+        int minSalary = 50_000;
+        int maxSalary = 500_000;
+        int age, salary, nameIndex, companyIndex;
 
         ArrayList<Employee> employees = new ArrayList<>();
 
         // создание объектов, наполнение списка
-
-        return employees;
-    }
+        for (int i = 0; i < num; i++) {
+            age = (int) (minAge + Math.random() * (maxAge - minAge));
+            salary = (int) (minSalary + Math.random() * (maxSalary - minSalary));
+            nameIndex = (int) (Math.random() * names.length);
+            companyIndex = (int) (Math.random() * companies.length);
+            employees.add(new Employee( names[nameIndex], companies[companyIndex], salary, age ));
+        }
+            return employees;
+        }
 }
---
+++ src/ru/itmo/homeworks/hw14/task2/comparator/SalaryComparator.java
@@ -0,0 +1,11 @@
+package ru.itmo.homeworks.hw14.task2.comparator;
+
+import ru.itmo.homeworks.hw14.task2.Employee;
+import java.util.Comparator;
+
+public class SalaryComparator implements Comparator<Employee> {
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        return 0;
+    }
+}
