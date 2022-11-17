7b8d4341c3a0415498167df9f1e848d6292778c5
Nikita Kurpatow <kurpatowph@yandex.ru>
2022-11-18 00:07:55 +0300

    homework: hw21 progress

---
+++ src/ru/itmo/homeworks/hw21/TCPClientIO.java
@@ -0,0 +1,63 @@
+package ru.itmo.homeworks.hw21;
+
+import ru.itmo.homeworks.hw21.common.Connection;
+import ru.itmo.homeworks.hw21.common.Message;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class TCPClientIO {
+    private final String ip;  // адрес серверной машины
+    private final int port;   // порт, на котором серверная программа ожидает клиента
+
+    private LocalDateTime receiptMessageTime;
+
+    public TCPClientIO(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите имя");
+        String name = scanner.nextLine();
+
+        while (true) {
+            System.out.println("Введите сообщение");
+            String text = scanner.nextLine();
+
+            if ("/exit".equalsIgnoreCase(text)) {
+                System.out.println("Закрытие приложения");
+                return;
+            }
+
+            Message message = new Message(name, text);
+
+            try (Connection<Message> connection = new Connection<>(new Socket(ip, port))) {
+                connection.sendMessage(message);
+                Message fromServer = connection.readMessage();
+                System.out.println("Сообщение от сервера: " + fromServer);
+                receiptMessageTime = LocalDateTime.now();
+            } catch (IOException | ClassNotFoundException e) {
+                System.out.println("Обработка IOException | ClassNotFoundException");
+            } catch (Exception e) {
+                System.out.println("Обработка Exception");
+            }
+
+            if ("/ping".equalsIgnoreCase(text)) {
+                System.out.println("Время обмена сообщениями с сервером: " + getPingTime(message) + " мс");
+            }
+        }
+    }
+
+    private int getPingTime(Message message) {
+        return (receiptMessageTime.getNano() - message.getDateTime().getNano()) / 1_000_000;
+    }
+
+    public static void main(String[] args) {
+        new TCPClientIO(TCPPropertiesIO.getClientIP(), TCPPropertiesIO.getClientPort()).run();
+    }
+}
---
+++ src/ru/itmo/homeworks/hw21/TCPServerIO.java
@@ -0,0 +1,70 @@
+package ru.itmo.homeworks.hw21;
+
+import ru.itmo.homeworks.hw21.common.Message;
+import ru.itmo.homeworks.hw21.common.Connection;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServerIO {
+    private final int port;
+
+    private final String sender = "Server";
+
+    private String messageText;
+
+    private int messageCounter;
+
+    public TCPServerIO(int port) {
+        this.port = port;
+        this.messageCounter = 0;
+    }
+
+    public void run() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Сервер запущен...");
+            while (true) {
+                // Связь устанавливается между Socket у клиента и Socket (который создается через метод accept) у сервера
+                Socket socket = serverSocket.accept(); // Пока клиент не подключится дальше инструкции не выполняются
+                // взаимодействие с клиентом (получение и передача информации)
+                Connection<Message> connection = new Connection<>(socket);
+                Message fromClient = connection.readMessage();
+                System.out.println("От клиента: " + fromClient);
+
+                messageCounter++;
+
+                if ("/help".equalsIgnoreCase(fromClient.getText())) {
+                    messageText = getHelpText();
+                } else if ("/count".equalsIgnoreCase(fromClient.getText())) {
+                    messageText = getCountText();
+                } else {
+                    messageText = "Сообщение от сервера";
+                }
+
+                Message message = new Message(sender, messageText);
+                connection.sendMessage(message);
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Обработка IOException или ClassNotFoundException");
+        }
+    }
+
+    private String getHelpText() {
+        String text = "\n" +
+                "Команда    /help    - отобразить справку по командам" + "\n" +
+                "Команда    /count   - отобразить количество сообщений, обработанное сервером" + "\n" +
+                "Команда    /ping    - отобразить время, за которое сообщение доходит до сервера и возвращается обратно"
+                + "\n" +
+                "Команда    /exit    - закрыть клиентское приложение";
+        return text;
+    }
+
+    private String getCountText() {
+        return "Количество сообщений, обработанных сервером, равно " + this.messageCounter;
+    }
+
+    public static void main(String[] args) {
+        new TCPServerIO(TCPPropertiesIO.getTCPPortFromString(args[0])).run();
+    }
+}
